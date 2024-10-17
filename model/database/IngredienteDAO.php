2955054a327d1461229b76e469cb5dfd90d13913
Matheus Cirne Varani <182320010@senacinformatica.br>
2024-10-16 21:35:54 -0300

    Função insert implementada

--- model/database/IngredienteDAO.php
+++ model/database/IngredienteDAO.php
@@ -6,8 +6,15 @@ class IngredienteDAO {
     public function list($id = null) {
         $where = ($id ? " WHERE id_ingredientes = $id":'');
         $query = "SELECT * FROM ingredientes$where";
-        $conect = DB::getConnection()->query($query);
-        $resultado = $conect->fetchAll();
+        $connect = DB::getConnection()->query($query);
+        $resultado = $connect->fetchAll();
         return $resultado;
+    }
+    public function insert(Ingrediente $obj) {
+        // SE USAR ARRAY ENVIAR PARAMETROS COMO VALUES (?, ?)
+        $query = "INSERT INTO ingredientes (id_ingredientes, descricao) VALUES (NULL, :descricao)";
+        $connect = DB::getConnection()->prepare($query);
+        $connect->execute(array(':descricao'=>$obj->descricao));
+        return $connect->rowCount()>0;
     }
 }
